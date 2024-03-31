dcbf24617c89ab6cde932a515224f8aa310c7beb
JuWi2517 <jan.richter@student.spsezatec.cz>
2024-03-31 23:54:12 +0200

    first

---
+++ src/HomePage.js
@@ -0,0 +1,17 @@
+import CryptoList from "./CryptoList";
+
+function HomePage() {
+  return (
+    <div>
+
+
+            <div className="content">
+                <CryptoList/>
+
+            </div>
+
+    </div>
+  );
+}
+
+export default HomePage;
